Fixed pointer benchmark code. Changed the plots to show the minimum run time out of 25 iterations instead of average and standard deviation of 15.

plot.py
@@ -3,6 +3,9 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+use_errs = False
+use_min = False
+
 results = []
 N_vals = []
 for fname in sorted(list(glob2.glob("result_*.txt"))):
@@ -10,23 +13,31 @@ for fname in sorted(list(glob2.glob("result_*.txt"))):
     results.append(np.loadtxt(fname, delimiter=", "))
 combined = np.stack(results)
 
-test_names = ["pointer", "boost::multi_array[][][]", "EnzoArray"]
+test_names = ["pointer", "boost::multi_array[][][]",
+              "boost::multi_array()",
+              "EnzoArray", "EnzoArray_neg"]
 data = {}
 N_vals = np.array(N_vals)
 idx = np.argsort(N_vals)
-fmts = ["ko:","rs","b."]
+fmts = ["ko:","rs","yo","g>", "b."]
 
 for i,name in enumerate(test_names):
     cur_data = combined[:,i,:]
-    data[name] = cur_data
-    print data[name]
+    if use_errs:
+        cur_vals = np.mean(cur_data,axis=1)
+        cur_err = np.std(cur_data,axis=1)
+    else:
+        cur_vals = np.amin(cur_data,axis=1)
+        cur_err = np.zeros_like(cur_vals)
+    data[name] = np.hstack([cur_vals[:,np.newaxis],cur_err[:,np.newaxis]])
+
 
 fig,ax = plt.subplots(2,1, figsize=(4,7),sharex=True)
 for name,fmt in zip(test_names,fmts):
     ax[0].errorbar(N_vals[idx],data[name][:,0][idx],
                    yerr=data[name][:,1][idx], fmt=fmt,label = name)
 ax[0].set_yscale("log")
-ax[0].set_xscale("log")
+ax[0].set_xscale("log",basex=2)
 ax[0].set_ylabel("$t(N^3)$")
 ax[0].legend()
 